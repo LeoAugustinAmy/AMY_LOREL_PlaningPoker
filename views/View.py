bb0de085fcb8363fe6ebf1883ca42393e8794bf4
stratengine1 <guillaume.lorel@outlook.fr>
2025-11-27 15:26:54 +0100

    feat: added link to new game page

--- views/View.py
+++ views/View.py
@@ -1,5 +1,5 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QStackedWidget
 
 """
 @class View
@@ -15,10 +15,13 @@ class View(QMainWindow) :
     @brief Constructor
     """
     super().__init__()
-
     uic.loadUi("ui/test.ui", self)
 
+    # Adding events
+    self.newGameButton.clicked.connect(lambda: self.switch_page(1))
+    self.backButton.clicked.connect(lambda: self.switch_page(0))
     self.quitButton.clicked.connect(self.quit_game)
+    self.switch_page(0)
   
   def quit_game(self):
       """
@@ -27,4 +30,15 @@ class View(QMainWindow) :
       """
       print("Quit !")
       self.close()
+
+  def switch_page(self, num_page=0):
+      """
+      @def switch_page
+      @brief Switch to the specified page
+      @param num_page : page number to switch to
+      """
+
+      self.stackedWidget.setCurrentIndex(num_page)
+      print(f"Switched to page {num_page}")
+
 
